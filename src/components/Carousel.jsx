1af89e0db33492d751caf195f2997b5ea3ea3b53
AadiswetTech <akshatm196@gmail.com>
2023-09-02 15:12:03 +0530

    updated code

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -2,48 +2,36 @@ import React, { useEffect, useState } from "react";
 const Carousel = ({ images }) => {
   const ImgSlide = Object.keys(images);
   const [current, changed] = useState(0);
-//   useEffect(() => {
-//     setTimeout(() => {
-//       changed((a) => {
-// console.log(a,'p')
-//    // set
-
-//        return  a > ImgSlide.length-2  ? 0 : (a + 1)} );
-
-//     },5000);
-//     console.log(current, 'c');
-//   }, [current]);
+  useEffect(() => {
+    setTimeout(() => {
+      changed((a) => 
+      {
+       return  a ===ImgSlide.length-1 ? 0 : (a + 1)} );
+      },3000);
+    }, [current]);
 
   const previous = () => {
-    // changed((b) => (b - 1 + ImgSlide.length) % ImgSlide.length);
-    // changed(current-1);
-    changed((b)=>b-1
-    
-    )
+   
+    changed((b) => (b===0 ? ImgSlide.length-1:b-1));
   };
+
   const next = () => {
-    // changed((c) => (c + 1) % ImgSlide.length);
-    changed((c) => (c + 1));
+    changed((c) => (c + ImgSlide.length)%ImgSlide.length);
   };
 
   return (
     <>
       <div className="slider-container">
-        <button className="slider-button" onClick={previous}>
-          prev
-        </button>
+        <button className="slider-button" onClick={previous}>prev</button>
         <div className="slider-images">
-          {/* {ImgSlide.map((name, index) => ( */}
-         { current < ImgSlide.length && current >-1 &&  <img
-            // key={current}
-            src={images[current].src}
-            //className={`slider-image ${index===current ? 'active' :''}`}
-          />}
-          {/* ))} */}
+         { 
+        // current < ImgSlide.length && current >-1 &&  
+        current <ImgSlide.length && current >=0 &&
+         <img src={images[current].src} />
+         }
         </div>
-        <button className="slider-button" onClick={next}>
-          next
-        </button>
+
+        <button className="slider-button" onClick={next}> next </button>
       </div>
     </>
   );
